c24f59f5d4ad104e2370c7a8d22ca9c9ee279279
James McDonald <james@toggen.com.au>
2018-04-20 21:23:34 +1000

    Run perl tidy on perm_saver.pl

--- perm_saver.pl
+++ perm_saver.pl
@@ -7,68 +7,62 @@ my $store_file = 'stored_perms.txt';
 
 $store_file = $ARGV[1] if ( defined $ARGV[1] );
 
+if ( !defined $ARGV[0] ) { usage() }
 
-if ( ! defined $ARGV[0] ){  usage()};
+store($store_file) if ( $ARGV[0] eq 'store' );
 
-store($store_file) if ($ARGV[0] eq 'store');
+restore($store_file) if ( $ARGV[0] eq 'restore' );
 
-restore($store_file) if ($ARGV[0] eq 'restore');
-
-sub usage 
-{
-	print "Saves directory names and permissions to '$store_file'\n";
-	print "Restores permissions from '$store_file'\n";
-	print $0 . ' [store|restore] <store_file>' . "\n";
-	exit;
+sub usage {
+    print "Saves directory names and permissions to '$store_file'\n";
+    print "Restores permissions from '$store_file'\n";
+    print $0 . ' [store|restore] <store_file>' . "\n";
+    exit;
 }
 
-
-sub restore($store_file){
+sub restore($store_file) {
 	print "Running RESTORE\n";
 
-	my $store_file = shift;
-	
-	 -f $store_file or die ( "$store_file does not exist!" );
+    my $store_file = shift;
 
-	open(FH, "<", $store_file) or die $!;
+    -f $store_file or die("$store_file does not exist!");
 
-	while(<FH>){
-		chomp;
-		my ( $dirname, $perms, $uid, $gid) = split(/:/);
+    open( FH, "<", $store_file ) or die $!;
 
-		print "Directory $dirname perms $perms\n";
+    while (<FH>) {
+        chomp;
+        my ( $dirname, $perms, $uid, $gid ) = split(/:/);
 
-		chmod oct($perms), $dirname;
-		chown $uid, $gid, $dirname;
+        print "Directory $dirname perms $perms\n";
 
-	}
+        chmod oct($perms), $dirname;
+        chown $uid, $gid, $dirname;
 
-	close(FH);
+    }
 
+    close(FH);
 }
 
-sub store
-{
+sub store {
+    my $store_file = shift;
 
-	my $store_file = shift;
+    print "Running STORE to $store_file\n";
+    my @dirs = `find .`;
 
-	print "Running STORE to $store_file\n";
-	my @dirs = `find .`;
+    open( FH, '>', $store_file ) or die $!;
 
-	open(FH, '>', $store_file) or die $!;
+    foreach (@dirs) {
+        chomp;
 
-
-	foreach(@dirs){
-		chomp;
-	#	print "$_" . "\n";
-		my @stats = stat($_); 
-		my $filemode = $stats[2];
-		my $uid = $stats[4];
-		my $gid = $stats[5];
-		printf FH "$_:%04o:$uid:$gid\n",  $filemode & 07777;
-
-	}
-
+        #	print "$_" . "\n";
+        my @stats    = stat($_);
+        my $filemode = $stats[2];
+        my $uid      = $stats[4];
+        my $gid      = $stats[5];
+        printf FH "$_:%04o:$uid:$gid\n", $filemode & 07777;
+    }
+    
 	close(FH);
+    
 	print "Stored to $store_file\n";
 }
